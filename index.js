2d7ddf29f7f87697d59d2493bb95cc497f003498
kunxin-chor <chorkunxin@yahoo.com>
2026-01-14 02:53:23 +0000

    Create connection

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require('express');
 const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
 require('dotenv').config();
+const { createPool } = require('mysql2/promise');
 
-let app = express();
+const app = express();
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 
 app.set('layout', 'layouts/base');
+
+const connection = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
