b29c1799e1efd7f40aca31c9ec5cfd8a8c2deb1c
kunxin-chor <chorkunxin@yahoo.com>
2026-01-14 03:06:30 +0000

    added landing page

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true}));
 app.set('layout', 'layouts/base');
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.render('landing')
 });
 
 app.listen(3000, () => {
